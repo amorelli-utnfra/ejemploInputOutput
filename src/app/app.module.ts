b6a1dee743601ca6c1cce7ab2c219db7c68ed35d
Augusto Morelli <a.morelli@sistemas-utnfra.com.ar>
2023-04-20 20:29:57 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PadreComponent } from './components/padre/padre.component';
+import { Hijo1Component } from './components/hijo1/hijo1.component';
+import { Hijo2Component } from './components/hijo2/hijo2.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    Hijo1Component,
+    Hijo2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
